1e0e2a4a6a9bf26bf5a5b69a377bbb84a18484da
Troy Dorman <troydorman@gmail.com>
2021-01-16 20:21:41 -0700

    file upload successfully works!

---
+++ routes/file_upload.js
@@ -0,0 +1,47 @@
+const db = require("../models");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+module.exports = function(app) {
+  const handleError = (err, res) => {
+    res
+      .status(500)
+      .contentType("text/plain")
+      .end("Oops! Something went wrong : " + err);
+  };
+
+  const upload = multer({
+    dest: "../uploads",
+    // you might also want to set some limits: https://github.com/expressjs/multer#limits
+  });
+
+  app.post(
+    "/upload",
+    upload.single("file" /* name attribute of <file> element in your form */),
+    (req, res) => {
+      const tempPath = req.file.path;
+      const targetPath = path.join(__dirname, "../uploads/image.png");
+
+      if (path.extname(req.file.originalname).toLowerCase() === ".png") {
+        fs.rename(tempPath, targetPath, (err) => {
+          if (err) return handleError(err, res);
+
+          res
+            .status(200)
+            .contentType("text/plain")
+            .end("File uploaded!");
+        });
+      } else {
+        fs.unlink(tempPath, (err) => {
+          if (err) return handleError(err, res);
+
+          res
+            .status(403)
+            .contentType("text/plain")
+            .end("Only .png files are allowed!");
+        });
+      }
+    }
+  );
+};
